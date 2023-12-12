d73f29869b3e89843c6152146e524b00c3d1b96c
mbilal-x <muhammadbx4@gmail.com>
2023-12-12 21:16:37 +0500

    load cell added

--- 1.1_com-h-w-bmi/com-h-w-bmi/com-h-w-bmi.ino
+++ 1.1_com-h-w-bmi/com-h-w-bmi/com-h-w-bmi.ino
@@ -8,6 +8,16 @@
 #define ECHO_PIN     D6  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
+// load cell
+byte pinData = D2;
+byte pinClk = D3;
+HX711 scale;
+// load cell => default weight for testing 
+// float weight_in_kilogram = 60;
+ //Parameter to calibrate weight
+float calibration_factor = 14450; //14255 
+
+
 // uuuuu
 float stand_height = 177.8;   //for 5feet 10inch stand just for testing agains my height
 float measured_distance = 0;
@@ -23,6 +33,13 @@ float weight = 60;
 void setup() {
   Serial.begin(9600); // Initialize serial communication
 
+  // load cell
+  scale.begin(pinData, pinClk);
+  //apply the calibration
+  scale.set_scale();
+  //initializing the tare. 
+  scale.tare();	//Reset the scale to 0gz
+
 }
 
 void loop() {
@@ -31,9 +48,14 @@ void loop() {
   delay(1000);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   measured_distance =  sonar.ping_cm();
 
+  // load cell
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  // Serial.print(scale.get_units(), 2);
+
+
   Serial.print(measured_distance);
   Serial.print(",");
-  Serial.println(weight);
+  Serial.println(scale.get_units(), 2);
 
   delay(1000); // Delay for readability and to avoid data flooding
 }
